935b6b330465f0177f33a6bd656dc06aa0faff33
NotValcake <r.valtorta@studenti.unibs.it>
2021-05-05 16:43:05 +0200

    aggiunti javadoc

--- src/TamaGolem.java
+++ src/TamaGolem.java
@@ -9,6 +9,11 @@ public class TamaGolem {
     private int hp = TamaConstants.V;
     private boolean alive = true;
 
+    /**
+     * inizializza un nuovo  TamaGolem
+     * @param name String contenente il nome da assegnare al nuovo tamagolem
+     * @param pietre ArrayList contenente il numero corrispondente all'elemento delle pietre ingurgitate dal TamaGolem
+     */
     public TamaGolem(String name, ArrayList<Integer> pietre){
         this.name = name;
         setPietre(pietre);
@@ -18,6 +23,10 @@ public class TamaGolem {
         return name;
     }
 
+    /**
+     * funzione che ogni turno lancia una pietra a rotazione
+     * @return la pietra lanciata
+     */
     public int throwPietre() {
         int to_be_thrown = pietre.peek();
         pietre.poll();
@@ -25,12 +34,20 @@ public class TamaGolem {
         return to_be_thrown;
     }
 
+    /**
+     * Aggiunge le pietre passate nell'ArrayList alla Queue "pietre"
+     * @param pietre ArrayList<Integer> contenente le pietre da assegnare al tamagolem
+     */
     public void setPietre(ArrayList<Integer> pietre) {
         for (Integer p: pietre) {
             this.pietre.offer(p);
         }
     }
 
+    /**
+     * diminuisce i punti vita del tamagolem e controlla se il tamagolem è in grado di proseguire la lotta
+     * @param hp l'ammontare di hp da sottrarre al tamagolem
+     */
     public void setHp(int hp){
         this.hp -= hp;
         this.alive = this.hp > 0;
