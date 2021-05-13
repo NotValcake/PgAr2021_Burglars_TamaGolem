e3fda40cc60f6a92cd1ec74f4f03cce53e89cffd
NotValcake <r.valtorta@studenti.unibs.it>
2021-05-13 11:26:48 +0200

    refactoring

--- src/Equilibrio.java
+++ src/Equilibrio.java
@@ -14,14 +14,16 @@ public class Equilibrio {
     }
     //TODO modificare algoritmo in modo che sia rispettata la condizione sup(W)<= V
 
-    /**crea valori in array random**/
-    private void  setEquilibrio(){
+    /**
+     * crea valori in array random
+     **/
+    private void setEquilibrio() {
         int[] sommeR = new int[equilibrio.getVertexNum()];
         int[] sommeC = new int[equilibrio.getVertexNum()];
 
         int prev = -1;
 
-        for (int i = 1; i < equilibrio.getVertexNum() -1; i++) {
+        for (int i = 1; i < equilibrio.getVertexNum() - 1; i++) {
             for (int j = 0; j < i; j++) {
                 if (randomRangeMaxMin(MAX, MIN) % 2 == 0) {
                     do {
@@ -39,30 +41,30 @@ public class Equilibrio {
             }
         }
 
-            for (int k = 0; k < sommeR.length; k++) {
-                for (int l = 0; l < sommeR.length; l++) {
-                    sommeR[k] += equilibrio.getVertex(l).get(k);
-                    sommeC[k] += equilibrio.getVertex(k).get(l);
-                }
+        for (int k = 0; k < sommeR.length; k++) {
+            for (int l = 0; l < sommeR.length; l++) {
+                sommeR[k] += equilibrio.getVertex(l).get(k);
+                sommeC[k] += equilibrio.getVertex(k).get(l);
             }
+        }
 
-            for (int j = 0; j < equilibrio.getVertexNum() -1; j++){
-                if ( sommeR[j] > sommeC[j])
-                   equilibrio.getVertex(j).set(equilibrio.getVertexNum()-1, sommeR[j] - sommeC[j]);
-                else equilibrio.getVertex(equilibrio.getVertexNum()-1).set(j, sommeC[j] - sommeR[j]);
+        for (int j = 0; j < equilibrio.getVertexNum() - 1; j++) {
+            if (sommeR[j] > sommeC[j])
+                equilibrio.getVertex(j).set(equilibrio.getVertexNum() - 1, sommeR[j] - sommeC[j]);
+            else equilibrio.getVertex(equilibrio.getVertexNum() - 1).set(j, sommeC[j] - sommeR[j]);
         }
     }
 
-    public static int randomRangeMaxMin(int max, int min){
+    public static int randomRangeMaxMin(int max, int min) {
         Random r = new Random();
-        return r.nextInt((max - min ) + 1) + min ; //max and min inclusive
+        return r.nextInt((max - min) + 1) + min; //max and min inclusive
     }
 
-    public int getEquilibrio(int from, int to){
+    public int getEquilibrio(int from, int to) {
         return equilibrio.getEdge(from, to);
     }
 
-    public int getSize(){
+    public int getSize() {
         return equilibrio.getVertexNum();
     }
 
